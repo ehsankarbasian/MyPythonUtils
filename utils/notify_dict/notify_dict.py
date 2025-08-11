e613c573e6502c8ac3c34233c5344fafd86a1abb
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-08-11 20:53:33 +0330

    substitute CacheLogger by a local LogCacher

--- utils/notify_dict/notify_dict.py
+++ utils/notify_dict/notify_dict.py
@@ -1,23 +1,36 @@
 
-import pathlib
-import sys
-path = str(pathlib.Path(__file__).parent.parent.parent.absolute())
-sys.path.append(path)
-
-from utils.notify_dict.logger_setup import setup_logger
-
-
-#REFACTOR: cache logs in NotifyDict and then use logger (less dependency and complexity)
 #TODO: add how to use
 
+
+#REFACTOR: Use as separate module
+class LogCacher:
+    
+    def __init__(self, prefix=None):
+        self._cache = []
+        self._prefix = prefix
+    
+    def cache_log(self, message):
+        if self._prefix:
+            message = f'{self._prefix} | {message}'
+        self._cache.append(message)
+    
+    # TODO: Strategy pattern
+    def print_all_cached_logs(self):
+        for line in self._cache:
+            print(line)
+    
+    def clear_cache(self):
+        self._cache = list()
+
+
 class NotifyDict(dict):
-    __slots__ = ["__cacheLogger",
+    __slots__ = ["__logCacher",
                  "__enable_default_value",
                  "__default_value"]
     
     def __init__(self, logfile_name, logger_pid, enable_default_value=False, default_value=None, *args, **kwargs):
-        cacheLogger = setup_logger(logfile_name=logfile_name, pid=logger_pid)
-        self.__cacheLogger = cacheLogger
+        prefix = f'{logger_pid} | {logfile_name}'
+        self.__logCacher = LogCacher(prefix=prefix)
         self.__enable_default_value = enable_default_value
         self.__default_value = default_value
         
@@ -26,27 +39,27 @@ class NotifyDict(dict):
     def __getitem__(self, __key):
         return_default_value = self.__enable_default_value and (__key not in self.keys())
         if return_default_value:
-            self.__cacheLogger.cache_log(f'__NO_SUCH_KEY__ (key:{__key}) | returning_default_value: {self.__default_value}')
+            self.__logCacher.cache_log(f'__NO_SUCH_KEY__ (key:{__key}) | returning_default_value: {self.__default_value}')
             return self.__default_value
         return super().__getitem__(__key)
     
     def __setitem__(self, key, value):
         previous_value = self.get(key, '__NOT_EXISTS__')
         message = f'The key "{key}" setted to "{value}" from "{previous_value}"'
-        self.__cacheLogger.cache_log(message)
+        self.__logCacher.cache_log(message)
         dict.__setitem__(self, key, value)
     
     def __delitem__(self, key):
         previous_value = self.get(key, '__NOT_EXISTS__')
         message = f'The key "{key}" deleted, previous_value: "{previous_value}"'
-        self.__cacheLogger.cache_log(message)
+        self.__logCacher.cache_log(message)
         dict.__delitem__(self, key)
     
     def write_all_cached_logs(self):
-        self.__cacheLogger.write_all_cached_logs()
+        self.__logCacher.print_all_cached_logs()
     
     def clear_all_cached_logs(self):
-        self.__cacheLogger.clear_cache()
+        self.__logCacher.clear_cache()
     
     def _wrap(method):
         def wrapper(self, *args, **kwargs):
